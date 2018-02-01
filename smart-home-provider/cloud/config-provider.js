911ee52dc1fb6bebe2c77bb53c0c69bff4f42150
Baskin Tapkan <baskint@gmail.com>
2018-01-31 22:32:31 -0600

    config update

--- smart-home-provider/cloud/config-provider.js
+++ smart-home-provider/cloud/config-provider.js
@@ -16,7 +16,7 @@ var Config = {};
 Config.devPortSmartHome = "3000";
 Config.smartHomeProviderGoogleClientId = "ZxjqWpsYj3"; // client id that Google will use
 Config.smartHomeProvideGoogleClientSecret = "hIMH3uWlMVrqa7FAbKLBoNUMCyLCtv"; // client secret that Google will use
-Config.smartHomeProviderApiKey = "<API_KEY>"; // client API Key generated on the console
+Config.smartHomeProviderApiKey = " AIzaSyA6WQB3PRZQRql3NO7wkwm8UOwuaNnS4Io"; // client API Key generated on the console
 Config.isLocal = false;
 
 function init() {
